340cba894148161915e885dc3625edbdd7a1ba4f
wanghui_java <wanghui_java@foxmail.com>
2020-03-15 23:18:39 +0800

    首页布局

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,35 @@
 <template>
-  <div>
-    <el-button type="info" @click="logout">退出</el-button>
-  </div>
+  <el-container class="home-container">
+    <el-header>
+      <div>
+        <img src="../assets/image/heima.png" alt="heima">
+        <span>电商后台管理系统</span>
+      </div>
+      <el-button type="info" @click="logout">退出</el-button>
+    </el-header>
+    <el-container>
+      <el-aside width="200px">
+        <el-menu
+          background-color="#333744"
+          text-color="#fff"
+          active-text-color="#ffd04b">
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>导航一</span>
+            </template>
+            <el-menu-item index="1-1">
+              <template slot="title">
+                <i class="el-icon-location"></i>
+                <span>选项1</span>
+              </template>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
@@ -17,5 +45,29 @@ export default {
 </script>
 
 <style lang="less" scoped>
-
+.home-container {
+  height: 100%;
+  .el-header {
+    background-color: #373d41;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding-left: 0;
+    > div {
+      color: #fff;
+      font-size: 20px;
+      display: flex;
+      align-items: center;
+      > span {
+        margin-left: 16px;
+      }
+    }
+  }
+  .el-aside {
+    background-color: #333744;
+  }
+  .el-main {
+    background-color: #eaedf1;
+  }
+}
 </style>
